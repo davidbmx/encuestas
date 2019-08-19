2f98003c98687bb5f01bed3b66b20cbfe5d1ecd3
David <David@MacBook-Pro-de-David.local>
2019-08-19 05:27:33 -0500

    remove bowserapi

--- config/settings/base.py
+++ config/settings/base.py
@@ -242,11 +242,6 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S%z',
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
     )
 }
 
