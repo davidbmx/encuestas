f72c57587b41a8c102588dc54044a172219d4370
David <David@MacBook-Pro-de-David.local>
2019-08-22 23:40:29 -0500

    add pagination

--- encuestas/backoffice/urls.py
+++ encuestas/backoffice/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import dashboard, encuesta, export_xls
+from .views import dashboard, encuesta, export_xls, encuestalist
 
 app_name = 'backoffice'
 
 urlpatterns = [
     path('dashboard/', dashboard, name='backoffice'),
     path('encuesta/<int:id_encuesta>/', encuesta, name='list_encuesta'),
+    path('encuestalist/<int:id_encuesta>/', encuestalist, name='list_encuesta_list'),
     path('encuesta/<int:id_encuesta>/exportar/', export_xls, name='exportar_encuesta'),
 ]
--- encuestas/backoffice/views.py
+++ encuestas/backoffice/views.py
@@ -15,11 +15,11 @@ def dashboard(request):
     total = DatosEncuestado.objects.all().count()
     return render(request, 'backoffice/dashboard.html', {'encuestas': encuestas, 'total_encuestas': DatosEncuestado, 'total': total})
 
+
 # @login_required
 # def encuesta(request, id_encuesta):
 #     encuesta = get_object_or_404(Encuesta, pk = id_encuesta)
 #     encuestas = encuestas = Encuesta.objects.all()
-#     # encuestado = DatosEncuestado.objects.filter(encuesta=encuesta)
 #     preguntas = Pregunta.objects.filter(encuesta=encuesta)
 
 #     datos = DatosEncuestado.objects.filter(encuesta=encuesta)
@@ -66,123 +66,81 @@ def dashboard(request):
 
 #     return render(request, 'backoffice/encuesta.html', context)
 
+
 @login_required
 def encuesta(request, id_encuesta):
     encuesta = get_object_or_404(Encuesta, pk = id_encuesta)
     encuestas = encuestas = Encuesta.objects.all()
     preguntas = Pregunta.objects.filter(encuesta=encuesta)
 
-    datos = DatosEncuestado.objects.filter(encuesta=encuesta)
-    nuevos_datos = []
-    for d in datos:
-        respuestas_normalizado = {}
-        for resp in d.respuesta.all():
-            if resp.pregunta.id not in respuestas_normalizado:
-                respuestas_normalizado[resp.pregunta.id] = []
-            titulo = ''
-            imagen = ''
-            if resp.opcion_respuesta:
-                titulo = resp.opcion_respuesta.titulo
-                if resp.opcion_respuesta.imagen:
-                    imagen = resp.opcion_respuesta.imagen.url
-
-            respuestas_normalizado[resp.pregunta.id].append({
-                'titulo': titulo,
-                'detalle': resp.detalle_respuesta,
-                'pregunta': resp.pregunta, 
-                'imagen': imagen
-            })
-
-        nuevos_datos.append({
-            'encuesta': d.encuesta,
-            'nombres': d.nombres,
-            'apellidos': d.apellidos,
-            'edad': d.edad,
-            'genero': d.genero,
-            'ciudad': d.ciudad,
-            'email': d.email,
-            'tiene_hijos': d.tiene_hijos,
-            'edad_hijos': d.edad_hijos,
-            'genero_hijo': d.genero_hijo,
-            'respuestas': respuestas_normalizado
-        })
-
     context = {
         'encuestas': encuestas,
         'encuesta': encuesta,
-        'encuestado': nuevos_datos,
-        'preguntas': preguntas
+        'preguntas': preguntas,
     }
 
     return render(request, 'backoffice/encuesta.html', context)
 
-# @login_required
-# def encuestalist(request, id_encuesta):
-#     encuesta = get_object_or_404(Encuesta, pk = id_encuesta)
-#     encuestas = encuestas = Encuesta.objects.all()
-#     preguntas = Pregunta.objects.filter(encuesta=encuesta)
+@login_required
+def encuestalist(request, id_encuesta):
+    encuesta = get_object_or_404(Encuesta, pk = id_encuesta)
 
-#     data = DatosEncuestado.objects.filter(encuesta=encuesta)
-#     page = request.GET.get('draw', 1)
-#     paginator = Paginator(data, 10)
-#     try:
-#         datos = paginator.page(page)
-#     except PageNotAnInteger:
-#         datos = paginator.page(1)
-#     except EmptyPage:
-#         datos = paginator.page(paginator.num_pages)
+    data = DatosEncuestado.objects.filter(encuesta=encuesta)
+    draw = request.GET.get('draw', 1)
+    length = request.GET.get('length', 10)
+    page = request.GET.get('start', 1)
 
-#     nuevos_datos = []
-#     for d in datos:
-#         respuestas_normalizado = {}
-#         for resp in d.respuesta.all():
-#             if resp.pregunta.id not in respuestas_normalizado:
-#                 respuestas_normalizado[resp.pregunta.id] = ''
-#             titulo = ''
-#             imagen = ''
-#             if resp.opcion_respuesta:
-#                 if resp.opcion_respuesta.titulo:
-#                     titulo = 'Opcion: {}\n'.format(resp.opcion_respuesta.titulo)
-#                 if resp.opcion_respuesta.imagen:
-#                     imagen = resp.opcion_respuesta.imagen.url
+    paginator = Paginator(data, length)
+    try:
+        datos = paginator.page((int(page) / 10)+1)
+    except PageNotAnInteger:
+        datos = paginator.page(1)
+    except EmptyPage:
+        datos = paginator.page(paginator.num_pages)
 
-#             # respuestas_normalizado[resp.pregunta.id].append({
-#             #     'titulo': titulo,
-#             #     'detalle': resp.detalle_respuesta,
-#             #     'pregunta': resp.pregunta.id, 
-#             #     'imagen': imagen
-#             # })
-#             detalle = ''
-#             if resp.detalle_respuesta:
-#                 detalle = 'Detalle: {} \n'.format(resp.detalle_respuesta)
-#             respuestas_normalizado[resp.pregunta.id] += '{}{} {}'.format(titulo, detalle, imagen)
-#                 # 'titulo': titulo,
-#                 # 'detalle': resp.detalle_respuesta,
-#                 # 'pregunta': resp.pregunta.id, 
-#                 # 'imagen': imagen
+    nuevos_datos = []
+    num = 0
+    for d in datos:
+        respuestas_normalizado = {}
+        for resp in d.respuesta.all():
+            num += 1
+            if resp.pregunta.id not in respuestas_normalizado:
+                respuestas_normalizado[resp.pregunta.id] = ''
+            titulo = ''
+            imagen = ''
+            if resp.opcion_respuesta:
+                if resp.opcion_respuesta.titulo:
+                    titulo = '<b>Opcion:</b> {}\n'.format(resp.opcion_respuesta.titulo)
+                if resp.opcion_respuesta.imagen:
+                    imagen = '<img style="width: 100%; margin:5px;" src="{}"/>'.format(resp.opcion_respuesta.imagen.url)
+
+            detalle = ''
+            if resp.detalle_respuesta:
+                detalle = '<b>Detalle:</b> {} \n'.format(resp.detalle_respuesta)
+            respuestas_normalizado[resp.pregunta.id] += '{}{} {}'.format(titulo, detalle, imagen)
 
 
-#         nuevos_datos.append({
-#             'encuesta': d.encuesta.nombre,
-#             'nombres': d.nombres,
-#             'apellidos': d.apellidos,
-#             'edad': d.edad,
-#             'genero': d.genero,
-#             'ciudad': d.ciudad.nombre,
-#             'email': d.email,
-#             'tiene_hijos': d.tiene_hijos,
-#             'edad_hijos': d.edad_hijos,
-#             'genero_hijo': d.genero_hijo,
-#             **respuestas_normalizado
-#         })
-#     response = {
-#         "draw": page,
-#         "recordsTotal": data.count(),
-#         "recordsFiltered": data.count(),
-#         'data': nuevos_datos
-#     }
+        nuevos_datos.append({
+            'encuesta': d.encuesta.nombre,
+            'nombres': d.nombres,
+            'apellidos': d.apellidos,
+            'edad': d.edad,
+            'genero': d.genero,
+            'ciudad': d.ciudad.nombre,
+            'email': d.email,
+            'tiene_hijos': 'SI' if d.tiene_hijos else 'NO',
+            'edad_hijos': d.edad_hijos,
+            'genero_hijo': d.genero_hijo,
+            **respuestas_normalizado
+        })
+    response = {
+        "draw": int(draw),
+        "recordsTotal": data.count(),
+        "recordsFiltered": data.count(),
+        'data': nuevos_datos
+    }
     
-#     return HttpResponse(json.dumps(response), content_type='application/json')
+    return HttpResponse(json.dumps(response), content_type='application/json')
 
 
 @login_required
